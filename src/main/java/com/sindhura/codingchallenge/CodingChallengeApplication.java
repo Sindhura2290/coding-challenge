d23afab763c2b9b994897518beb4e95cdfd57cb4
Sindhura2290 <sindhuraailneni@gmail.com>
2020-09-23 13:45:32 -0500

    Coding challenge

--- src/main/java/com/sindhura/codingchallenge/CodingChallengeApplication.java
+++ src/main/java/com/sindhura/codingchallenge/CodingChallengeApplication.java
@@ -11,11 +11,12 @@ import static com.sindhura.codingchallenge.challenge.Hierarchy.hierarchy;
 public class CodingChallengeApplication {
 
 	public static void main(String[] args) {
+		SpringApplication.run(com.sindhura.codingchallenge.CodingChallengeApplication.class, args);
 		String input = "null, 0, garndpa|0,1,son|0,2,daughter|1,3,grandkid|1,4,grandkid|2,5,grandkid|5,6,greatgrandkid";
 
 		Node node = hierarchy(input);
 		System.out.println(display(node));
-		SpringApplication.run(CodingChallengeApplication.class, args);
+
 	}
 
 }
